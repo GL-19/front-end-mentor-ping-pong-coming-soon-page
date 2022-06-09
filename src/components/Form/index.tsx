da2751e99ec38b6a6ffd43ac65b3ac1b90f9d8cf
GL-19 <loiolagl19@gmail.com>
2022-06-09 11:56:45 -0300

    App finalizada

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,35 @@
-import { FormContainer, Input, Button } from "./styles";
+import { FormEvent, useState } from "react";
+import { FormContainer, Input, Button, ErrorMsg } from "./styles";
+
+const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
 export function Form() {
+	const [email, setEmail] = useState("");
+	const [invalidEmail, setInvalidEmail] = useState(false);
+
+	function handleSubmit(event: FormEvent<InnerHTML>): void {
+		event.preventDefault();
+
+		if (!regex.test(email)) {
+			setInvalidEmail(true);
+		} else {
+			setInvalidEmail(false);
+			alert("Enviado com sucesso");
+		}
+	}
+
 	return (
-		<FormContainer action="">
-			<Input type="text" placeholder="Your email address" isActive={false} />
-			<Button>Notify Me</Button>
+		<FormContainer action="submit" onSubmit={handleSubmit}>
+			<Input
+				placeholder="Your email address"
+				isActive={invalidEmail}
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+
+			{invalidEmail && <ErrorMsg>Please provide a valid email address</ErrorMsg>}
+
+			<Button type="submit">Notify Me</Button>
 		</FormContainer>
 	);
 }
--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -6,9 +6,10 @@ interface InputProps {
 }
 
 export const Input = styled.input<InputProps>`
-	width: 60%;
+	width: 100%;
 	height: 3rem;
 	padding: 0 1.5rem;
+	grid-area: input;
 
 	border-radius: 5rem;
 	border: 0.125rem solid
@@ -24,44 +25,54 @@ export const Input = styled.input<InputProps>`
 	:focus {
 		outline: none;
 	}
-
-	@media screen and (max-width: ${mediaQuery.tablet}) {
-		width: 100%;
-	} ;
 `;
 
 export const Button = styled.button`
-	width: 25%;
+	width: 100%;
 	height: 3rem;
+	grid-area: button;
 
 	color: white;
 	font-weight: 600;
 
 	background-color: ${({ theme }) => theme.colors.blue};
+	border: transparent;
 	border-radius: 5rem;
 
 	&:hover {
 		opacity: 0.75;
 	}
+`;
 
-	@media screen and (max-width: ${mediaQuery.tablet}) {
-		width: 100%;
-	} ;
+export const ErrorMsg = styled.p`
+	grid-area: error;
+
+	color: ${({ theme }) => theme.colors.lightRed};
+	font-size: 0.75rem;
+	margin-left: 1.5rem;
+	width: 100%;
 `;
 
 export const FormContainer = styled.form`
 	width: 100%;
 	min-height: 2rem;
 
-	display: flex;
-	flex-direction: row;
+	display: grid;
+	grid-template-columns: 60% 25%;
+	grid-template-areas:
+		"input button"
+		"error  .";
 	justify-content: center;
 	align-items: center;
 	gap: 1rem;
+	row-gap: 0.5rem;
 
 	@media screen and (max-width: ${mediaQuery.tablet}) {
+		display: flex;
 		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 		min-height: 5rem;
 		gap: 0.5rem;
-	} ;
+	}
 `;
